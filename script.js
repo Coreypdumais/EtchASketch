5c81252521d34437dbe4d7a317752994543a43e9
unknown <coreypdumais@gmail.com>
2022-02-19 13:54:35 -0500

    used dev.to article to create a pixel art generator in place of the original etch a sketch model

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 console.log("Testing connection")
+
+const container = document.querySelector('.container')
+const sizeE1 = document.querySelector('.size');
+const color = document.querySelector('.color');
+const resetBtn = document.querySelector('.btn');
+
+//getting the value of the size input
+
+let size = sizeE1.value
+
+//we will only color our pixels if draw is set to true
+
+let draw = false;
+
+function populate(size) {
+    //updating the --size css variable
+    container.style.setProperty('--size', size)
+    for (let i=0; i <size * size;i++) {
+        const div = document.createElement('div')
+        div.classList.add('pixel')
+
+        div.addEventListener('mouseover', function(){
+            if(!draw) return
+            div.style.backgroundColor = color.value
+        })
+
+        div.addEventListener('mousedown', function(){
+            //we don't need to check if draw is true here
+            //because if we click on a pixel that means we want to draw that pixel
+            div.style.backgroundColor = color.value;
+        })
+
+        container.appendChild(div)
+    }
+}
+
+//set draw to true when users presses down on the mouse
+window.addEventListener("mousedown", function(){
+    draw=true;
+})
+
+//set draw to false when user releases the mouse
+window.addEventListener("mouseup", function(){
+    draw=false;
+})
+
+function reset(){
+    container.innerHTML =''
+    populate(size)
+}
+
+resetBtn.addEventListener('click', reset)
+
+sizeE1.addEventListener('keyup', function(){
+    size=sizeE1.value
+    reset()
+})
+
+
+populate(size)
